0f3cdecdfd3fa2e339cd415ca1b74394b631edb7
cnickels21 <cnickels_21@yahoo.com>
2020-04-09 21:24:07 -0500

    get from cache and api working in location handler

--- server.js
+++ server.js
@@ -47,188 +47,170 @@ function setLocationInCache(location) {
     .catch(error => console.error(error));
 }
 
-// function getLocationFromCache(city) {
-//   const SQL = `
-//   SELECT *
-//   FROM Locations
-//   WHERE search_query = $1
-//   LIMIT 1
-//   `;
-//   const parameters = [city];
+function getLocationFromCache(city) {
+  const SQL = `
+  SELECT *
+  FROM Locations
+  WHERE search_query = $1
+  LIMIT 1
+  `;
+  const parameters = [city];
 
-//   return client.query(SQL, parameters);
-
-
-
-  // .then(results => {
-  //   console.log(results);
-  //   let {rowCount, rows} = results;
-  //   if (rowCount === 0) {
-  //     response.send({
-  //       error: true,
-  //       message: 'No cities in database'
-  //     })
-  //   }
-  //   else {
-  //     response.send({
-  //       error: false,
-  //       results: rows,
-  //     })
-  //   }
-  // })
-  // .catch(error => {
-  //   console.log(error);
-  //   errorHandler(error, request, response);
-  // })
-
-
-// }
+  return client.query(SQL, parameters);
+}
 
 // Route Handler
 function locationHandler(request, response) {
 
   const city = request.query.city;
 
-  // const locationFromCache = getLocationFromCache(city);
+  getLocationFromCache(city)
+  .then(result => {
+    let {rowCount, rows} = result;
+    if (rowCount > 0) {
+      response.send(rows[0]);
+    } else {
+      return getLocationFromAPI(city, response);
+    }
+  })
+}
 
-  // if (locationFromCache) {
-  //   response.send(locationFromCache);
-  // } else {
-    const url = 'https://us1.locationiq.com/v1/search.php';
 
-    superagent.get(url)
-      .query({
-        key: process.env.GEO_KEY,
-        q: city,
-        format: 'json'
-      })
-      .then(locationResponse => {
-        let geoData = locationResponse.body;
-        const location = new Location(city, geoData);
-        setLocationInCache(location)
-          .then(() => {
-            console.log('Location has been cached', location);
-            response.send(location);
-          })
-      })
-      .catch(error => {
-        console.log(error);
-        errorHandler(error, request, response);
-      })
-  }
-// }
-
-  app.get('/weather', weatherHandler);
-
-  function weatherHandler(request, response) {
-
-    // const weatherData = require('./data/darksky.json');
-
-    const weather = request.query.search_query;
-    const url = 'http://api.weatherbit.io/v2.0/current';
-
-    superagent.get(url)
-      .query({
-        key: process.env.WEATHER_KEY,
-        city: weather,
-        format: 'json'
-      })
-      .then(weatherResponse => {
-        let weatherData = weatherResponse.body;
-        let dailyResults = weatherData.data.map(dailyWeather => {
-          return new Weather(dailyWeather);
+function getLocationFromAPI(city, response) {
+  const url = 'https://us1.locationiq.com/v1/search.php';
+  superagent.get(url)
+    .query({
+      key: process.env.GEO_KEY,
+      q: city,
+      format: 'json'
+    })
+    .then(locationResponse => {
+      let geoData = locationResponse.body;
+      const location = new Location(city, geoData);
+      setLocationInCache(location, response)
+        .then(() => {
+          console.log('Location has been cached', location);
+          response.send(location);
         })
-        response.send(dailyResults);
-      })
-      .catch(error => {
-        console.log(error);
-        errorHandler(error, request, response);
-      })
-
-    // const weather = request.query;  TODO: get lat/lon
-
-  }
-
-  app.get('/trails', trailHandler);
-
-  function trailHandler(request, response) {
-
-    const lat = request.query.latitude;
-    const lon = request.query.longitude;
-    const url = 'https://www.hikingproject.com/data/get-trails';
-
-    superagent.get(url)
-      .query({
-        key: process.env.TRAILS_KEY,
-        lat: lat,
-        lon: lon,
-        format: 'json'
-      })
-      .then(trailsResponse => {
-        let trailsData = trailsResponse.body;
-        let trailsResults = trailsData.trails.map(allTrails => {
-          return new Trails(allTrails);
-        })
-        response.send(trailsResults);
-      })
-      .catch(error => {
-        console.log(error);
-        errorHandler(error, request, response);
-      })
-  }
-
-  // Has to happen after everything else
-  app.use(notFoundHandler);
-  // Has to happen after the error might have occurred
-  app.use(errorHandler); // Error Middleware
-
-  // Helper Functions
-
-  function errorHandler(error, request, response, next) {
-    console.log(error);
-    response.status(500).json({
-      error: true,
-      message: error.message,
-    });
-  }
-
-  function notFoundHandler(request, response) {
-    response.status(404).json({
-      notFound: true,
-    });
-  }
-
-  client.connect()
-    .then(() => {
-      console.log('Database connected.')
-      app.listen(PORT, () => console.log(`Listening on ${PORT}`));
     })
     .catch(error => {
-      throw `Something went wrong: ${error}`;
+      console.log(error);
+      errorHandler(error, request, response);
+    })
+}
+
+
+app.get('/weather', weatherHandler);
+
+function weatherHandler(request, response) {
+
+  // const weatherData = require('./data/darksky.json');
+
+  const weather = request.query.search_query;
+  const url = 'http://api.weatherbit.io/v2.0/current';
+
+  superagent.get(url)
+    .query({
+      key: process.env.WEATHER_KEY,
+      city: weather,
+      format: 'json'
+    })
+    .then(weatherResponse => {
+      let weatherData = weatherResponse.body;
+      let dailyResults = weatherData.data.map(dailyWeather => {
+        return new Weather(dailyWeather);
+      })
+      response.send(dailyResults);
+    })
+    .catch(error => {
+      console.log(error);
+      errorHandler(error, request, response);
     })
 
-  function Location(city, geoData) {
-    this.search_query = city;
-    this.formatted_query = geoData[0].display_name;
-    this.latitude = parseFloat(geoData[0].lat);
-    this.longitude = parseFloat(geoData[0].lon);
-  }
+  // const weather = request.query;  TODO: get lat/lon
 
-  function Weather(weatherData) {
-    this.forecast = weatherData.weather.description;
-    this.time = new Date(weatherData.ob_time).toDateString();
-  }
+}
 
-  function Trails(trailsData) {
-    this.name = trailsData.name;
-    this.location = trailsData.location;
-    this.length = trailsData.length;
-    this.stars = trailsData.stars;
-    this.starVotes = trailsData.starVotes;
-    this.summary = trailsData.summary;
-    this.trail_url = trailsData.url;
-    this.conditions = trailsData.conditionDetails;
-    this.condition_date = new Date(trailsData.conditionDate).toDateString();
-  }
+app.get('/trails', trailHandler);
+
+function trailHandler(request, response) {
+
+  const lat = request.query.latitude;
+  const lon = request.query.longitude;
+  const url = 'https://www.hikingproject.com/data/get-trails';
+
+  superagent.get(url)
+    .query({
+      key: process.env.TRAILS_KEY,
+      lat: lat,
+      lon: lon,
+      format: 'json'
+    })
+    .then(trailsResponse => {
+      let trailsData = trailsResponse.body;
+      let trailsResults = trailsData.trails.map(allTrails => {
+        return new Trails(allTrails);
+      })
+      response.send(trailsResults);
+    })
+    .catch(error => {
+      console.log(error);
+      errorHandler(error, request, response);
+    })
+}
+
+// Has to happen after everything else
+app.use(notFoundHandler);
+// Has to happen after the error might have occurred
+app.use(errorHandler); // Error Middleware
+
+// Helper Functions
+
+function errorHandler(error, request, response, next) {
+  console.log(error);
+  response.status(500).json({
+    error: true,
+    message: error.message,
+  });
+}
+
+function notFoundHandler(request, response) {
+  response.status(404).json({
+    notFound: true,
+  });
+}
+
+client.connect()
+  .then(() => {
+    console.log('Database connected.')
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  })
+  .catch(error => {
+    throw `Something went wrong: ${error}`;
+  })
+
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = parseFloat(geoData[0].lat);
+  this.longitude = parseFloat(geoData[0].lon);
+}
+
+function Weather(weatherData) {
+  this.forecast = weatherData.weather.description;
+  this.time = new Date(weatherData.ob_time).toDateString();
+}
+
+function Trails(trailsData) {
+  this.name = trailsData.name;
+  this.location = trailsData.location;
+  this.length = trailsData.length;
+  this.stars = trailsData.stars;
+  this.starVotes = trailsData.starVotes;
+  this.summary = trailsData.summary;
+  this.trail_url = trailsData.url;
+  this.conditions = trailsData.conditionDetails;
+  this.condition_date = new Date(trailsData.conditionDate).toDateString();
+}
 
 // app.listen(PORT, () => console.log(`Listening on ${PORT}`));
